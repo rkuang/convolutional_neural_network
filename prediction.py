a8e7c7807ce8e2f7be6efb2a1e20de728c0ae855
Ricky Kuang <rkuang26@gmail.com>
2018-06-02 16:06:30 -0700

    tensorflow cnn

--- prediction.py
+++ prediction.py
@@ -1,38 +1,115 @@
-# Starter code for CS 165B HW4
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from PIL import Image
 
-"""
-Implement the testing procedure here. 
+# Imports
+import numpy as np
+import tensorflow as tf
 
-Inputs:
-    Given the folder named "hw4_test" that is put in the same directory of your "predictio.py" file, like:
-    - Main folder
-        - "prediction.py"
-        - folder named "hw4_test" (the exactly same as the uncompressed hw4_test folder in Piazza)
-    Your "prediction.py" need to give the following required output.
+tf.logging.set_verbosity(tf.logging.INFO)
 
-Outputs:
-    A file named "prediction.txt":
-        * The prediction file must have 10000 lines because the testing dataset has 10000 testing images.
-        * Each line is an integer prediction label (0 - 9) for the corresponding testing image.
-        * The prediction results must follow the same order of the names of testing images (0.png – 9999.png).
-    Notes: 
-        1. The teaching staff will run your "prediction.py" to obtain your "prediction.txt" after the competition ends.
-        2. The output "prediction.txt" must be the same as the final version you submitted to the CodaLab, 
-        elsewise you will be given 0 score for your hw4.
+def cnn_model_fn(features, labels, mode):
+  """Model function for CNN."""
+  # Input Layer
+  input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
 
+  # Convolutional Layer #1
+  conv1 = tf.layers.conv2d(
+      inputs=input_layer,
+      filters=32,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
 
-**!!!!!!!!!!Important Notes!!!!!!!!!!**
-    To open the folder "hw4_test" or load other related files, 
-    please use open('./necessary.file') instaed of open('some/randomly/local/directory/necessary.file').
+  # Pooling Layer #1
+  pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
 
-    For instance, in the student Jupyter's local computer, he stores the source code like:
-    - /Jupyter/Desktop/cs165B/hw4/prediction.py
-    - /Jupyter/Desktop/cs165B/hw4/hw4_test
-    If he use os.chdir('/Jupyter/Desktop/cs165B/hw4/hw4_test'), this will cause an IO error 
-    when the teaching staff run his code under other system environments.
-    Instead, he should use os.chdir('./hw4_test').
+  # Convolutional Layer #2 and Pooling Layer #2
+  conv2 = tf.layers.conv2d(
+      inputs=pool1,
+      filters=64,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+  pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
 
+  # Dense Layer
+  pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+  dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+  dropout = tf.layers.dropout(
+      inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
 
-    If you use your local directory, your code will report an IO error when the teaching staff run your code,
-    which will cause 0 socre for your hw4.
-"""
+  # Logits Layer
+  logits = tf.layers.dense(inputs=dropout, units=10)
+
+  predictions = {
+      # Generate predictions (for PREDICT and EVAL mode)
+      "classes": tf.argmax(input=logits, axis=1),
+      # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+      # `logging_hook`.
+      "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+  }
+
+  if mode == tf.estimator.ModeKeys.PREDICT:
+    return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+  # Calculate Loss (for both TRAIN and EVAL modes)
+  loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+
+  # Configure the Training Op (for TRAIN mode)
+  if mode == tf.estimator.ModeKeys.TRAIN:
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+    train_op = optimizer.minimize(
+        loss=loss,
+        global_step=tf.train.get_global_step())
+    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+  # Add evaluation metrics (for EVAL mode)
+  eval_metric_ops = {
+      "accuracy": tf.metrics.accuracy(
+          labels=labels, predictions=predictions["classes"])}
+  return tf.estimator.EstimatorSpec(
+      mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+def main(unused_argv):
+  # Load training and eval data
+  mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+  train_data = mnist.train.images  # Returns np.array
+  train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+  eval_data = mnist.test.images  # Returns np.array
+  eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+
+  # Create the Estimator
+  mnist_classifier = tf.estimator.Estimator(
+      model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model")
+
+  # Set up logging for predictions
+  # Log the values in the "Softmax" tensor with label "probabilities"
+  tensors_to_log = {"probabilities": "softmax_tensor"}
+  logging_hook = tf.train.LoggingTensorHook(
+      tensors=tensors_to_log, every_n_iter=50)
+
+  # Train the model
+  train_input_fn = tf.estimator.inputs.numpy_input_fn(
+      x={"x": train_data},
+      y=train_labels,
+      batch_size=100,
+      num_epochs=None,
+      shuffle=True)
+  mnist_classifier.train(
+      input_fn=train_input_fn,
+      steps=20000,
+      hooks=[logging_hook])
+
+  # Evaluate the model and print results
+  eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+      x={"x": eval_data},
+      y=eval_labels,
+      num_epochs=1,
+      shuffle=False)
+  eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+  print(eval_results)
+
+if __name__ == "__main__":
+  tf.app.run()

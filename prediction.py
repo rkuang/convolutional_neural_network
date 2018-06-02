0e9418ab6d8c3e7908495a233fae5f1b0e173c50
Ricky Kuang <rkuang26@gmail.com>
2018-06-02 13:08:26 -0700

    initial commit

---
+++ prediction.py
@@ -0,0 +1,38 @@
+# Starter code for CS 165B HW4
+
+"""
+Implement the testing procedure here. 
+
+Inputs:
+    Given the folder named "hw4_test" that is put in the same directory of your "predictio.py" file, like:
+    - Main folder
+        - "prediction.py"
+        - folder named "hw4_test" (the exactly same as the uncompressed hw4_test folder in Piazza)
+    Your "prediction.py" need to give the following required output.
+
+Outputs:
+    A file named "prediction.txt":
+        * The prediction file must have 10000 lines because the testing dataset has 10000 testing images.
+        * Each line is an integer prediction label (0 - 9) for the corresponding testing image.
+        * The prediction results must follow the same order of the names of testing images (0.png – 9999.png).
+    Notes: 
+        1. The teaching staff will run your "prediction.py" to obtain your "prediction.txt" after the competition ends.
+        2. The output "prediction.txt" must be the same as the final version you submitted to the CodaLab, 
+        elsewise you will be given 0 score for your hw4.
+
+
+**!!!!!!!!!!Important Notes!!!!!!!!!!**
+    To open the folder "hw4_test" or load other related files, 
+    please use open('./necessary.file') instaed of open('some/randomly/local/directory/necessary.file').
+
+    For instance, in the student Jupyter's local computer, he stores the source code like:
+    - /Jupyter/Desktop/cs165B/hw4/prediction.py
+    - /Jupyter/Desktop/cs165B/hw4/hw4_test
+    If he use os.chdir('/Jupyter/Desktop/cs165B/hw4/hw4_test'), this will cause an IO error 
+    when the teaching staff run his code under other system environments.
+    Instead, he should use os.chdir('./hw4_test').
+
+
+    If you use your local directory, your code will report an IO error when the teaching staff run your code,
+    which will cause 0 socre for your hw4.
+"""
